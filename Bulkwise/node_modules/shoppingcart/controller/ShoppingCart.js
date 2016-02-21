751c2018a11b3e5dece8f3b6fd52fbefa579c79e
System Administrator <root@Harmans-MacBook-Pro.local>
2016-02-21 22:30:51 +0000

    Update shopping cart in bulkwize

--- Bulkwise/node_modules/shoppingcart/controller/ShoppingCart.js
+++ Bulkwise/node_modules/shoppingcart/controller/ShoppingCart.js
@@ -8,7 +8,20 @@
 var express = require('express');
 var shoppingcart = express.Router();
 var ShoppingCartModel = require('../model/ShoppingCartModel.js');
+var _ = require('underscore');
 
+/**
+ * get Shoppingcart
+ */
+shoppingcart.get('/user/:userid', function (req, res, next) {
+
+    ShoppingCartModel.get(req.params['userid'], function (error, result) {
+        if (error) {
+            return res.status(400).send(error);
+        }
+        res.send(result);
+    });
+});
 
 /**
  * Save Shoppingcart
@@ -21,7 +34,7 @@ shoppingcart.put('/', function (req, res, next) {
 
     populateSessionDetails(req, data);
 
-    ShoppingCartModel.get( data.customer_id, function (error, result) {
+    ShoppingCartModel.get(data.customer_id, function (error, result) {
         if (error) {
             console.log('No previous cart details for the user');
         } else {
@@ -33,22 +46,26 @@ shoppingcart.put('/', function (req, res, next) {
                     var match = false;
                     var pdtFromDB = result.data[0].bulkwize.products;
 
-                    for (var i = 0; i < pdtFromSite.length; i++) {
+                    _.each(pdtFromSite, function (ele) {
 
-                        pdtFromDB.forEach(function (ele, index) {
+                        var prod = _.findWhere(pdtFromDB, {'id': ele.id});
+                        if (prod !== null && !_.isUndefined(prod)) {
+                            _.each(ele.variants, function (ele) {
 
-                            if (ele.id === pdtFromSite[i].id) {
-                                ele.quantity = pdtFromSite[i].quantity;
-                                match = true;
-                            }
+                                var variant = _.findWhere(prod.variants, {'sku_id': ele.sku_id});
+                                if (variant != null) {
+                                    variant.quantity = ele.quantity;
+                                } else {
+                                    prod.variants.push(ele);
+                                }
 
-                        });
-
-                        if (!match) {
-                            pdtFromDB.push(pdtFromSite[i]);
+                            });
+                        } else {
+                            pdtFromDB.push(ele);
                         }
 
-                    }
+                    });
+
                     data = result.data[0].bulkwize;
                 }
             }
@@ -63,11 +80,10 @@ shoppingcart.put('/', function (req, res, next) {
 });
 
 
-
 /**
  * delete products from Shoppingcart
  */
-shoppingcart.delete('/', function (req, res, next) {
+shoppingcart.delete('/product', function (req, res, next) {
 
 
     var data = req.body;
@@ -75,7 +91,7 @@ shoppingcart.delete('/', function (req, res, next) {
 
     populateSessionDetails(req, data);
 
-    ShoppingCartModel.get( data.customer_id, function (error, result) {
+    ShoppingCartModel.get(data.customer_id, function (error, result) {
         if (error) {
             console.log('No previous cart details for the user');
         } else {
@@ -86,18 +102,63 @@ shoppingcart.delete('/', function (req, res, next) {
 
                     var pdtFromDB = result.data[0].bulkwize.products;
 
-                    for (var i = 0; i < pdtFromSite.length; i++) {
+                    _.each(pdtFromSite, function (ele) {
 
-                        pdtFromDB =  pdtFromDB.filter(function (ele) {
+                        var pdt = _.findWhere(pdtFromDB,{'id': ele.id});
+                        pdtFromDB = _.without(pdtFromDB, pdt);
 
-                            return ele.id != pdtFromSite[i].id;
-
-                        });
-
-                    }
+                    });
                     result.data[0].bulkwize.products = pdtFromDB;
                     data = result.data[0].bulkwize;
                 }
+            }
+        }
+
+        //saving final data
+        saveCart(data, res);
+
+    });
+
+
+});
+
+
+/**
+ * delete products from Shoppingcart
+ */
+shoppingcart.delete('/variants', function (req, res, next) {
+
+
+    var data = req.body;
+
+
+    populateSessionDetails(req, data);
+
+    ShoppingCartModel.get(data.customer_id, function (error, result) {
+        if (error) {
+            console.log('No previous cart details for the user');
+        } else {
+            if (result.data != null && result.data.length > 0) {
+                var pdtFromSite = data.products;
+
+                var pdtFromDB = result.data[0].bulkwize.products;
+
+                _.each(pdtFromSite, function (ele) {
+
+                    var prod = _.findWhere(pdtFromDB, {'id': ele.id});
+
+                    if (prod !== null) {
+                        _.each(ele.variants, function (ele) {
+
+                            var variant = _.findWhere(prod.variants, {'sku_id': ele.sku_id});
+                            prod.variants = _.without(prod.variants, variant);
+
+                        });
+                    }
+
+                    data = result.data[0].bulkwize;
+
+                });
             }
         }
 
@@ -131,19 +192,23 @@ var populateSessionDetails = function (req, data) {
 
 };
 
+var populateVariants = function (dbVariants, siteVariants) {
+    console.log(dbVariants + " db variants")
+    if (siteVariants != null) {
+        siteVariants.forEach(function (ele, index) {
+            console.log("site variant  " + ele.sku_id);
+            dbVariants.forEach(function (dbvariant, index) {
+                console.log("dbvariant dbvariant  " + dbvariant);
+                if (ele.sku_id == dbvariant.sku_id) {
+                    console.log("matched");
+                    ele.quantity = dbvariant.quantity;
+                }
+            });
+        });
 
-/**
- * get Shoppingcart
- */
-shoppingcart.get('/user/:userid', function (req, res, next) {
+    }
+}
 
-    ShoppingCartModel.get(req.params['userid'], function (error, result) {
-        if (error) {
-            return res.status(400).send(error);
-        }
-        res.send(result);
-    });
-});
 
 // export product module
 module.exports = shoppingcart;
