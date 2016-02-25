0611daf07eb879a070609a5ca737dddc31e86bce
ashishsmital <ashishsmital@gmail.com>
2016-02-25 23:16:27 +0530

    Added promotion module to return carousel URLs

--- Bulkwise/index.js
+++ Bulkwise/index.js
@@ -18,6 +18,7 @@ var category = require('category/controller/Category.js');
 var products = require('products/controller/Products.js');
 var shoppingcart = require('shoppingcart/controller/ShoppingCart.js');
 var supplier = require('supplier/controller/Supplier.js');
+var promotion = require('promotion/controller/Promotion.js');
 
 
 
@@ -75,6 +76,7 @@ app.use('/category', category);
 app.use('/products', products);
 app.use('/shoppingcart', shoppingcart);
 app.use('/supplier', supplier);
+app.use('/promotion', promotion);
 console.log("The dir name is -- "+ __dirname+'../appcontent');
 app.use('/appcontent',express.static(__dirname+'/../appcontent'));
 
