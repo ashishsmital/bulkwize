fe42d1e886d74bb2d782968ae17ce73e62673482
ashishsmital <ashishsmital@gmail.com>
2016-03-10 09:32:13 +0530

    Trying to fix CORS issue with loginSuccess redirect

--- Bulkwise/main.js
+++ Bulkwise/main.js
@@ -32,27 +32,11 @@ main.get('/registration', function(req, res, next) {
 
 
 
-main.post('/login', function(req, res, next){
-	
-	// Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-	
-	passport.authenticate('local', {
+main.post('/login', passport.authenticate('local', {
             successRedirect: '/loginSuccess',
             failureRedirect: '/loginFailure'
-    });
-}
-    
+    })
+   
 );
 
 
