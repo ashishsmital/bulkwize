09dfe29590d9ec797a9bc08146c631547f030990
ghanavel <jerinavel@gmail.com>
2016-03-12 18:05:46 +0530

    In the register page, currently there are 2 fields, billing address and shipping address, these needs to be broken down.

---
+++ MobileFiles/www/js/controllers/registerCtrl.js
@@ -0,0 +1,73 @@
+/**
+ * Created by ghanavela on 3/12/2016.
+ */
+app.controller('RegisterCtrl', function($scope, $rootScope, $ionicLoading, $http, $ionicPopup, $state){
+    $scope.step1 = true;
+    $scope.step2 = false;
+    $scope.step3 = false;
+    $scope.step4 = false;
+    $scope.goNext = function(next){
+       for(var i=1; i <= 4; i++){
+           if(i === next ){
+               $scope["step"+i] = true;
+           }else{
+               $scope["step"+i] = false;
+           }
+       }
+    };
+    $scope.submit = function(valid, value){
+        console.log(value);
+        debugger;
+        if(valid){
+            debugger;
+            $ionicLoading.show({
+                content: 'Loading',
+                animation: 'fade-in',
+                showBackdrop: true,
+                maxWidth: 200,
+                showDelay: 0
+            });
+
+            $http({
+                method: 'POST',
+                url: 'http://52.73.228.44:8080/user/',
+                data:
+                {
+                    "mobileNumber":value.mob,
+                    "shopName":value.mob,
+                    "firstname":value.shopname,
+                    "password":value.password,
+                    "lastname":value.lastname,
+                    "pan":value.panumber,
+                    "email":value.email,
+                    "shopAddress":value.shopaddress,
+                    "deliveryAddress":"1234",
+                    "type":"com.bulkwise.User",
+                    "id":"com.bulkwise.User::"+value.mob
+
+                },
+            }).then(function successCallback(response) {
+                console.log(response.data.data.cas);
+                if(response.data.data.cas){
+                    var alertPopup = $ionicPopup.alert({
+                        title: 'Info',
+                        template: 'Thanks for register with us'
+                    });
+
+                    alertPopup.then(function(res) {
+                        console.log(res);
+                        if(res == true){
+                            $state.go('app.login');
+                        }
+                    });
+                }
+                $ionicLoading.hide();
+            }, function errorCallback(data) {
+                console.log(data);
+                $ionicLoading.hide();
+            });
+        }
+
+    }
+
+});
