2efdf5173defdd8a10686a79ee31a5179ffc832e
ghanavel <jerinavel@gmail.com>
2016-03-24 12:57:21 +0530

    User details form split.

--- MobileFiles/www/js/controllers/UserdetailsCtrl.js
+++ MobileFiles/www/js/controllers/UserdetailsCtrl.js
@@ -6,8 +6,55 @@ app.controller('UserDetailsCtrl', function($scope, $rootScope, $ionicLoading, $h
                                            $ionicHistory,AuthServices,EnvConfig){
 
 
+
+
+    $scope.step1 = true;
+    $scope.step2 = false;
+    $scope.step3 = false;
+    $scope.step4 = false;
+
+    $scope.reg = {};
+    $scope.reg.shopCity = 'Bangalore';
+    $scope.reg.shopState = 'Karnataka';
+
+    $scope.reg.delCity = 'Bangalore';
+    $scope.reg.delState = 'Karnataka';
+
+    /*
+     $scope.step1 = true;
+     $scope.step2 = true;
+     $scope.step3 = true;
+     $scope.step4 = true;
+    */
+
+    $scope.reg = {};
+
+    $scope.goNextPrev = function(step){
+        for(var i=1; i <= 4; i++){
+            if(i === step ){
+                $scope["step"+i] = true;
+            }else{
+                $scope["step"+i] = false;
+            }
+        }
+    };
+
+
+
+
+
     AuthServices.getUserDetailsById($stateParams.id.split("::")[1]).then(function(data){
         $scope.userDetails = data;
+        $scope.reg.companyname = $scope.userDetails.shopName;
+        $scope.reg.mob =  $scope.userDetails.mobileNumber;
+        $scope.reg.password = $scope.userDetails.password;
+        $scope.reg.confirmpassword = $scope.userDetails.password;
+        $scope.reg.firstname = $scope.userDetails.firstName;
+        $scope.reg.lastname = $scope.userDetails.lastName;
+        $scope.reg.panumber = $scope.userDetails.pan;
+        $scope.reg.email = $scope.userDetails.email;
+
+        //$scope.reg.companyname = ;
         console.log($scope.userDetails);
     });
 
