32ceb420857ce4d57467b64e32156e721d96dec9
ashishsmital <ashishsmital@gmail.com>
2016-03-27 12:26:26 +0530

    Enahanced creation os supplier only if it does not already exist

--- Supplier/model/SupplierModel.js
+++ Supplier/model/SupplierModel.js
@@ -29,31 +29,34 @@ SupplierModel.save = function(data, callback) {
 	var supplierJsonObject ={
 		"id":0,
 		"type" : "com.bulkwize.Supplier",
-		"supplier_firstname" : data.supplierFirstName,
-		"supplier_lastname" : data.supplierLastName,
-		"supplier_business name" : data.supplierBusinessName,
-		"supplier_list of holidays":data.supplierListOfholidays,
-		"supplier_lead_time_to_deliver_in_working_days" : data.supplierLeadTimeToDeliver, 
+		"supplier_firstname" : data.supplier_firstname,
+		"supplier_lastname" : data.supplier_lastname,
+		"supplier_business_name" : data.supplier_business_name,
+		"supplier_manufacturer_name" : data.supplier_manufacturer_name,
+		"supplier_list_of_holidays":data.supplier_list_of_holidays,
+		"supplier_lead_time_to_deliver_in_working_days" : data.supplier_lead_time_to_deliver_in_working_days, 
 		"supplier_taxes_in_percentage":{"vat":data.vat, "sales_tax":data.salesTax},
-		"supplier_business address": data.supplierBusinessAddress,
-		"supplier_warehouse-details":{
-			"supplier_warehouse address":data.supplierWareHouseDetails,
-			"supplier_warehouse_timings":data.supplierWareHouseTimings,
-			"supplier_warehouse_contact_details":data.supplierWareHouseContactDetails
+		"supplier_business_address": data.supplier_business_address,
+		"supplier_warehouse_details":{
+			"supplier_warehouse_address":data.supplier_warehouse_address,
+			"supplier_warehouse_timings":data.supplier_warehouse_timings,
+			"supplier_warehouse_contact_details":data.supplier_warehouse_contact_details
 		},
-		"supplier_contact_details":data.supplierContactDetails
+		"supplier_contact_details":data.supplier_contact_details
 	}
 
 
-
-    db.counter("supplierCounter",1, function(err, res) {
+	SupplierModel.getByBusinessName("supplier_business_name",data.supplier_business_name, function(error,result){
+		if(error){
+			console.log("No supplier found byt the given business name, hence it needs to be added");
+			db.counter("supplierCounter",1, function(err, res) {
 		  		  if (err) {
 		  			  console.log('Unable to fetch supplier counter & hence can not create dummy supplier', err);
 					  return;
 		  		  }
 
 		  		  console.log('Creating a supplier with id...', res.value);
-				  supplierJsonObject.id=res.value;
+				  supplierJsonObject.id="com.bulkwize.Supplier::"+res.value;
 				  // create dummy supplier
 							db.insert('suppl::'+res.value, supplierJsonObject , function(err, result) {
 								  if (err) {
@@ -65,7 +68,12 @@ SupplierModel.save = function(data, callback) {
 								  callback(null, {message: 'success', data: result});
 								});
 					
-		  		});		
+		  		});
+		}else{
+			console.log("Supplier found by the given business name, hence skipping adding this supplier");
+		}
+	});
+    		
 
  }
 
@@ -108,6 +116,29 @@ SupplierModel.getAllById = function(attribute,value,callback) {
             callback(error, null);
             return;
         }
+        callback(null, {message: 'success', data: result});
+        return;
+    });
+}
+
+/**
+ * SupplierModel get By Supplier business name
+ *
+ * @param data
+ *          supplier json
+ * @param callback
+ *          callback for http response
+ */
+SupplierModel.getByBusinessName = function(attribute,value,callback) {
+	console.log("Retrieving supplier by business name " + value);
+    var query = N1qlQuery.fromString("select * from "+db._name+" where "+attribute+"="+value +"and type='com.bulkwize.Supplier'");
+
+    db.query(query, function(error, result) {
+        if (error) {
+            callback(error, null);
+            return;
+        }
+		console.log("Supplier retrieved by business name " + value + " is -- " + JSON.stringify(result));
         callback(null, {message: 'success', data: result});
         return;
     });
