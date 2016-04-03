696576d7b32b7c198f4ddc7cb6ddde7b0b2173c6
Devaraj <devaraj.cs@gmail.com>
2016-04-03 12:03:41 +0530

    Category
    1.Get invoice for order number
    
    URL : http://localhost:8080/order/:orderNo/invoice

--- Order/controller/Order.js
+++ Order/controller/Order.js
@@ -10,6 +10,7 @@ var order = express.Router();
 var orderModel = require('../model/OrderModel.js');
 var _ = require('underscore');
 var request = require('request');
+var config = require('../config/invoice-config.json');
 
 /**
  * post order
@@ -36,6 +37,23 @@ order.get('/', function (req, res, next) {
         res.send(result);
     });
 });
+
+
+/**
+ * get invoice for order number
+ */
+order.get('/:orderNo/invoice', function (req, res, next) {
+    console.log("Get invoice for orderId  - " + req.params['orderNo']);
+    orderModel.getAllOrders("id",req.params['orderNo'], function (error, result) {
+        if (error) {
+            return res.status(400).send(error);
+        }else{
+            _.extend(result.data[0].Bulkwize, {'bulkwize-address': config.bulkwizeAddress});
+        }
+        res.send(result);
+    });
+});
+
 
 // export product module
 module.exports = order;
