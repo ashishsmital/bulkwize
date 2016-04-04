53444fd065b70f0d41135c4aa3d74a5889c1ac49
ghanavel <jerinavel@gmail.com>
2016-04-04 20:02:03 +0530

    latest build

--- MobileFiles/platforms/android/build/intermediates/assets/debug/www/js/controllers/BrandListCtrl.js
+++ MobileFiles/platforms/android/build/intermediates/assets/debug/www/js/controllers/BrandListCtrl.js
@@ -4,15 +4,17 @@
 
 app.controller('BrandListCtrl', function($scope, $stateParams, $http, $ionicLoading, $rootScope, $state , EnvConfig){
     $ionicLoading.show();
+    $scope.envHost = EnvConfig.HOST.substring(0, EnvConfig.HOST.length - 1);
     $http({
         method: 'GET',
-        url: EnvConfig.HOST+'produtcs/topdiscounts/all'
+        url: EnvConfig.HOST+'products/topdiscounts/all'
     }).then(function successCallback(data) {
-        console.log(data);
-        debugger;
+        $scope.brands =  _.chunk(data.data.data , 2);
+        console.log($scope.brands);
+        $ionicLoading.hide();
+
      }, function errorCallback(data) {
         console.log(data);
-        debugger;
         $ionicLoading.hide();
     });
 
