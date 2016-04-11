2687c19e6eb203eaa5f868eac8b330b9a00b7f5f
dm3627 <dm3627@gmail.com>
2016-04-11 09:23:38 +0530

    Updated after changing bulkwize.com to auto

--- MobileFiles/www/js/app.js
+++ MobileFiles/www/js/app.js
@@ -18,7 +18,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'ngMessa
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		  ga('create', 'UA-75906058-2', 'bulkwize.com');
+		  ga('create', 'UA-75906058-2', "auto");
 		  //ga('send', 'pageview');
 
 
