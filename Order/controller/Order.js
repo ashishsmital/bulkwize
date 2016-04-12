9173fc18c4591ffe3b7f5a556d3efb90f7ddd9b6
Devaraj <devarajs.cbe@gmail.com>
2016-04-12 01:35:44 +0100

    updated code for variant alone invoice
    updated code for persisting and updating invoice number in orders itself

--- Order/controller/Order.js
+++ Order/controller/Order.js
@@ -9,6 +9,8 @@ var express = require('express');
 var order = express.Router();
 var orderModel = require('../model/OrderModel.js');
 var _ = require('underscore');
+var uuid = require('uuid');
+var moment = require('moment');
 var request = require('request');
 var config = require('../config/invoice-config.json');
 var smsConfig = require('../config/sms-config.json');
@@ -19,32 +21,32 @@ var http = require("http");
  * post order
  */
 order.post('/create', function (req, res, next) {
-	console.log("Create order for user with id - " + req.user.user);
+    console.log("Create order for user with id - " + req.user.user);
     orderModel.createOrder(req.user.user, function (error, result) {
         if (error) {
             return res.status(400).send(error);
-        } 
+        }
         res.send(result);
-		 // send order sms to end consumer
-		var smsOptions = smsConfig.smsOptions;
-		console.log("Printing order object before sending SMS " + JSON.stringify(result));
-		smsOptions.path=smsOptions.path+result.data.customer_id+"&Msg=Your%20order%20number%20with%20Bulkwize&is%20"+result.data.id;
-		console.log("SMS options is " + JSON.stringify(smsOptions));
-		var req = http.request(smsOptions, function (res) {
-		  var chunks = [];
+        // send order sms to end consumer
+        var smsOptions = smsConfig.smsOptions;
+        console.log("Printing order object before sending SMS " + JSON.stringify(result));
+        smsOptions.path=smsOptions.path+result.data.customer_id+"&Msg=Your%20order%20number%20with%20Bulkwize&is%20"+result.data.id;
+        console.log("SMS options is " + JSON.stringify(smsOptions));
+        var req = http.request(smsOptions, function (res) {
+            var chunks = [];
 
-		  res.on("data", function (chunk) {
-			chunks.push(chunk);
-		  });
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
 
-		  res.on("end", function () {
-			var body = Buffer.concat(chunks);
-			console.log(body.toString());
-		  });
-		});
+            res.on("end", function () {
+                var body = Buffer.concat(chunks);
+                console.log(body.toString());
+            });
+        });
 
-		req.end();
-		// end of send SMS
+        req.end();
+        // end of send SMS
 
     });
 });
@@ -53,11 +55,11 @@ order.post('/create', function (req, res, next) {
  * get order
  */
 order.get('/', function (req, res, next) {
-	console.log("Get order for customer_id  - " + req.user.user);
+    console.log("Get order for customer_id  - " + req.user.user);
     orderModel.getAllOrders("customer_id",req.user.user, function (error, result) {
         if (error) {
             return res.status(400).send(error);
-        } 
+        }
         res.send(result);
     });
 });
@@ -75,12 +77,11 @@ order.get('/:orderNo/invoice', function (req, res, next) {
             return res.status(400).send(error);
         }else{
             pdtItems = [],subtot = 0;
-            var invoiceNumber = result.data[0].Bulkwize.id;//TODO should be auto generated
+            var invoiceNumber =uuid.v4().substring(1,5) +"-"+ moment(new Date()).utcOffset("+05:30").format('DDMMYYYY');//TODO should be auto generated
             _.extend(result.data[0].Bulkwize, {'bulkwize-address': config.bulkwizeAddress});
             result.data[0].Bulkwize.products.forEach(function(obj,i){
-              obj.productVariants.forEach(function (variant,i)
+                obj.productVariants.forEach(function (variant,i)
                 {
-
                     amt=variant.productMRPUnit*variant.productOrderedQty*variant.productCountInCase;
                     pdtItems.push({
                         description: variant.productMaterialDescription,
@@ -99,7 +100,7 @@ order.get('/:orderNo/invoice', function (req, res, next) {
             input = {
                 currencyFormat: "₹%d",
                 invoice_number: invoiceNumber,
-                order_number:invoiceNumber, //TODO check with Ashish tomorrow.
+                order_number:result.data[0].Bulkwize.id, //TODO check with Ashish tomorrow.
                 orderDate: orderDate.toISOString().
                 replace(/T/, ' ').
                 replace(/\..+/, ''),
@@ -120,20 +121,128 @@ order.get('/:orderNo/invoice', function (req, res, next) {
             var invoice = new Invoice();
             var fileStream = fs.createWriteStream(__dirname+invoiceNumber+'-invoice.pdf');
             invoice.generatePDFStream(input).pipe(fileStream).on('finish', function () {  // finished
-                //_.extend(result.data[0].Bulkwize,{'invoiceFilePath':fileStream.relative})
+                if(!result.data[0].Bulkwize.invoice){
+                    _.extend(result.data[0].Bulkwize,{'invoice':[{'invoiceNumber':invoiceNumber+'-invoice.pdf'}]});
+                }else{
+                    result.data[0].Bulkwize.invoice.push({'invoiceNumber':invoiceNumber+'-invoice.pdf'});
+                }
                 //res.send(result);
+                orderModel.updateOrder(result.data[0].Bulkwize.id,result.data[0].Bulkwize, function (error, result) {
 
-                fs.readFile(__dirname+invoiceNumber+'-invoice.pdf', function(error, content) {
                     if (error) {
-                        res.writeHead(500);
-                        res.end();
-                    }
-                    else {
-                        res.writeHead(200, { 'Content-Type': 'application/pdf',  'Content-Disposition': 'inline; filename='+invoiceNumber+'-invoice.pdf'});
-                        res.end(content, 'utf-8');
+                        return res.status(400).send(error);
+                    } else {
+                        fs.readFile(__dirname+invoiceNumber+'-invoice.pdf', function(error, content) {
+                            if (error) {
+                                res.writeHead(500);
+                                res.end();
+                            }
+                            else {
+
+                                res.writeHead(200, { 'Content-Type': 'application/pdf',  'Content-Disposition': 'inline; filename='+invoiceNumber+'-invoice.pdf'});
+                                res.end(content, 'utf-8');
+                            }
+                        });
+
                     }
                 });
+            });
 
+            console.log('Invoice end');
+        }
+
+
+
+    });
+});
+
+
+/**
+ * get invoice for order number
+ */
+order.get('/:orderNo/invoice/:variantId/variants', function (req, res, next) {
+    console.log("Get invoice for orderId  - " + req.params['orderNo']);
+    var _dirname = process.cwd()+"/"
+    var variantId = req.params['variantId']
+    orderModel.getAllOrders("id",req.params['orderNo'], function (error, result) {
+
+        if (error) {
+            return res.status(400).send(error);
+        }else{
+            pdtItems = [],subtot = 0;
+            var invoiceNumber = uuid.v4().substring(1,5) +"-"+ moment(new Date()).utcOffset("+05:30").format('DDMMYYYY');//TODO should be auto generated
+            _.extend(result.data[0].Bulkwize, {'bulkwize-address': config.bulkwizeAddress});
+            result.data[0].Bulkwize.products.forEach(function(obj,i){
+                obj.productVariants.forEach(function (variant,i)
+                {
+                    if(variant.sku_id == variantId) {
+                        amt = variant.productMRPUnit * variant.productOrderedQty * variant.productCountInCase;
+                        pdtItems.push({
+                            description: variant.productMaterialDescription,
+                            quantity: variant.productOrderedQty,
+                            rate: variant.productMRPUnit,
+                            amount: amt,
+                            vat: variant.productVATPercentage,
+                            countInCase: variant.productCountInCase,
+                        });
+                        subtot += parseInt(amt);
+                    }
+                });
+            });
+
+            orderDate = new Date(result.data[0].Bulkwize.updatedAt);
+            invoiceDate = new Date()
+            input = {
+                currencyFormat: "₹%d",
+                invoice_number: invoiceNumber,
+                order_number:result.data[0].Bulkwize.id, //TODO check with Ashish tomorrow.
+                orderDate: orderDate.toISOString().
+                replace(/T/, ' ').
+                replace(/\..+/, ''),
+                invoiceDate: invoiceDate.toISOString().
+                replace(/T/, ' ').
+                replace(/\..+/, ''),
+                from_name: 'Bulkwise',
+                client_name: result.data[0].Bulkwize.shipping_address.address1,
+                items: pdtItems,
+                subtotal:subtot,
+                tax:0,
+                shipping:0,
+                paid:0,
+                balance:subtot,
+
+            };
+
+            var invoice = new Invoice();
+            var fileStream = fs.createWriteStream(__dirname+invoiceNumber+'-invoice.pdf');
+            invoice.generatePDFStream(input).pipe(fileStream).on('finish', function () {  // finished
+                if(!result.data[0].Bulkwize.invoice){
+                    _.extend(result.data[0].Bulkwize,{'invoice':[{'invoiceNumber':invoiceNumber+'-invoice.pdf'}]});
+                }else{
+                    result.data[0].Bulkwize.invoice.push({'invoiceNumber':invoiceNumber+'-invoice.pdf'});
+                }
+
+                //res.send(result);
+
+                orderModel.updateOrder(result.data[0].Bulkwize.id,result.data[0].Bulkwize, function (error, result) {
+
+                    if (error) {
+                        return res.status(400).send(error);
+                    } else {
+                        fs.readFile(__dirname+invoiceNumber+'-invoice.pdf', function(error, content) {
+                            if (error) {
+                                res.writeHead(500);
+                                res.end();
+                            }
+                            else {
+
+                                res.writeHead(200, { 'Content-Type': 'application/pdf',  'Content-Disposition': 'inline; filename='+invoiceNumber+'-invoice.pdf'});
+                                res.end(content, 'utf-8');
+                            }
+                        });
+
+                    }
+                });
 
             });
 
