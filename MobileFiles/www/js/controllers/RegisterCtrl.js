36c45c1805d9bc5189a091eb0966ce0b3f47e9af
ghanavel <jerinavel@gmail.com>
2016-04-18 19:50:17 +0530

    vat issue fix

--- MobileFiles/www/js/controllers/RegisterCtrl.js
+++ MobileFiles/www/js/controllers/RegisterCtrl.js
@@ -29,6 +29,48 @@ app.controller('RegisterCtrl', function($scope, $rootScope, $ionicLoading, $http
 	$scope.reg = {};
 */
 
+
+    $scope.toggleItem = function(item){
+        item.checked = !item.checked;
+    };
+
+    $scope.data = {
+        showDelete: false
+    };
+
+    $scope.itemButtons = [
+        {
+            text: 'Edit',
+            type: 'button-assertive',
+            onTap: function(item) {
+                alert('Edit Item: ' + item.id);
+            }
+        },
+        {
+            text: 'Share',
+            type: 'button-calm',
+            onTap: function(item) {
+                alert('Share Item: ' + item.id);
+            }
+        }
+    ];
+
+    $scope.onItemDelete = function(item) {
+        $scope.items.splice($scope.items.indexOf(item), 1);
+    };
+
+    $scope.items = [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+        { id: 4 },
+        { id: 5 },
+        { id: 6 },
+        { id: 7 },
+        { id: 8 },
+        { id: 9 },
+        { id: 10 }
+    ];
     
 
     $scope.goNextPrev = function(step){
