ac2bfe7b85d3feef98d622e383fc17513076381e
ashishsmital <ashishsmital@gmail.com>
2016-05-02 08:47:46 +0530

    Was incorrectly checking DB variant quantity instead of request variant quantity

--- ShoppingCart/controller/ShoppingCart.js
+++ ShoppingCart/controller/ShoppingCart.js
@@ -149,7 +149,7 @@ shoppingcart.put('/', function (req, res, next) {
                                 var variant = _.findWhere(prod.variants, {'sku_id': ele.sku_id});
                                 if (variant != null) { // existing variant coming from ui, so update its quantity if > 0 or delete that variant from array 
 									console.log("Product variant quantity is -- " + variant.quantity);
-									if(variant.quantity == 0){
+									if(ele.quantity == 0){
 										prod.variants = _.without(prod.variants, variant);
 									}else{
 										variant.quantity = ele.quantity;
